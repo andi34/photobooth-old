8be5dbe88833fd27dd1d552383bb6d9bbb2a682f
Andreas Blaesius <andi@unlegacy-android.org>
2019-12-25 11:40:05 +0100

    Allow PhotoCollage from device cam
    Change-Id: I77ba11c8a45bec97ce0f4bae10f38e001dd6cfde

--- api/savePic.php
+++ api/savePic.php
@@ -3,7 +3,9 @@ header('Content-Type: application/json');
 
 require_once('../lib/config.php');
 
-if ($config['file_format_date']) {
+if (!empty($_POST['file']) && preg_match('/^[a-z0-9_]+\.jpg$/', $_POST['file'])) {
+    $file = $_POST['file'];
+} elseif ($config['file_format_date']) {
     $file = date('Ymd_His').'.jpg';
 } else {
     $file = md5(time()).'.jpg';
@@ -11,14 +13,57 @@ if ($config['file_format_date']) {
 
 $filename_tmp = $config['foldersAbs']['tmp'] . DIRECTORY_SEPARATOR . $file;
 
+if (!isset($_POST['style'])) {
+    die(json_encode([
+        'error' => 'No style provided'
+    ]));
+}
 
-$data = $_POST['canvasimg'];
-list($type, $data) = explode(';', $data);
-list(, $data)      = explode(',', $data);
-$data = base64_decode($data);
+if ($_POST['style'] === 'photo') {
+    $data = $_POST['canvasimg'];
+    list($type, $data) = explode(';', $data);
+    list(, $data)      = explode(',', $data);
+    $data = base64_decode($data);
 
 
-file_put_contents($filename_tmp, $data);
+    file_put_contents($filename_tmp, $data);
+} elseif ($_POST['style'] === 'collage') {
+    if (!is_numeric($_POST['collageNumber'])) {
+        die(json_encode([
+            'error' => 'No or invalid collage number provided',
+        ]));
+    }
+
+    $number = $_POST['collageNumber'] + 0;
+
+    if ($number > 3) {
+        die(json_encode([
+            'error' => 'Collage consists only of ' . $config['collage_limit'] . ' pictures',
+        ]));
+    }
+
+    $basename = substr($filename_tmp, 0, -4);
+    $filename = $basename . '-' . $number . '.jpg';
+
+    $data = $_POST['canvasimg'];
+    list($type, $data) = explode(';', $data);
+    list(, $data)      = explode(',', $data);
+    $data = base64_decode($data);
+
+
+    file_put_contents($filename, $data);
+
+    die(json_encode([
+        'success' => 'collage',
+        'file' => $file,
+        'current' => $number,
+        'limit' => $config['collage_limit'],
+    ]));
+} else {
+    die(json_encode([
+        'error' => 'Invalid photo style provided',
+    ]));
+}
 
 // send imagename to frontend
 echo json_encode([
