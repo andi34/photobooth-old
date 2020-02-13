jacques42/photobooth

Configurable image preview while post-processing

resources/lang/de.js
@@ -80,6 +80,7 @@ const L10N = {
     'show_fork': 'Fork Logo anzeigen',
     'general_cntdwn_time': 'Countdown Timer in Sekunden',
     'general_cheese_time': 'Cheeeeeeeese!-Timer in Millisekunden',
+    'showimage_whenprocessing': 'Bild laden bevor Filter berechnet werden',
     'previewFromCam': 'Vorschau aus der Gerätekamera anzeigen',
     'previewCamTakesPic': 'Gerätekamera zur Bildaufnahme verwenden',
     'newest_first': 'Neueste Bilder zuerst anzeigen',

resources/lang/en.js
@@ -80,6 +80,7 @@ const L10N = {
     'show_fork': 'Show Fork Badge',
     'general_cntdwn_time': 'Countdown timer in seconds',
     'general_cheese_time': 'Cheeeeeeeese!-Timer in milliseconds',
+    'showimage_whenprocessing': 'Preload and show image during filter processing',
     'previewFromCam': 'See preview by device cam',
     'previewCamTakesPic': 'Device cam takes picture',
     'newest_first': 'Show latest images first',

resources/lang/es.js
@@ -80,6 +80,7 @@ const L10N = {
     'show_fork': 'Muestre el banner de la página',
     'general_cntdwn_time': 'Cuenta regresiva en segundos',
     'general_cheese_time': '¡Definir el tiempo para el patataaaaas!en milisegundos.',
+    'showimage_whenprocessing': 'Preload image before processing',
     'previewFromCam': 'Ver el vistazo por cámara',
     'previewCamTakesPic': 'Device cam takes picture',
     'newest_first': 'Primero muestre las últimas páginas',

resources/lang/fr.js
@@ -80,6 +80,7 @@ const L10N = {
     'show_fork': 'Affichez le badge de la page Github',
     'general_cntdwn_time': 'Compte à rebours en secondes',
     'general_cheese_time': 'Définir le temps pour le Cheeeeeeeese! en millisecondes',
+    'showimage_whenprocessing': 'Preload image before processing',
     'previewFromCam': 'Voir l\'aperçu par caméra',
     'previewCamTakesPic': 'Device cam takes picture',
     'newest_first': 'Affichez les dernières images en premier',

resources/lang/gr.js
@@ -80,6 +80,7 @@ const L10N = {
     'show_fork': 'Εμφανίστε το λογότυπο Fork',
     'general_cntdwn_time': 'Μετρητής αντίστροφης μέτρησης σε δευτερόλεπτα',
     'general_cheese_time': 'Cheeeeeeeese!-Μετρητής αντίστροφης σε χιλιοστά του δευτερολέπτου',
+    'showimage_whenprocessing': 'Preload image before processing',
     'previewFromCam': 'Προβολή προεπισκόπησης από τη φωτογραφική μηχανή tablet',
     'previewCamTakesPic': 'Device cam takes picture',
     'newest_first': 'Προβολή των τελευταίων φωτογραφιών πρώτα',

andi34/photobooth-old

Change-Id: I3e9dcdf91dd830baa8f3c854b57dc8e34340ab81

resources/lang/de.js
@@ -84,6 +84,7 @@ const L10N = {
     'previewFromCam': 'Vorschau aus der Gerätekamera anzeigen',
     'previewCamTakesPic': 'Gerätekamera zur Bildaufnahme verwenden',
     'newest_first': 'Neueste Bilder zuerst anzeigen',
+    'image_preview_before_processing': 'Bild laden bevor Filter berechnet werden',
     'folders_images': 'Bild Ordner',
     'folders_thumbs': 'Thumbnail Ordner',
     'folders_qrcodes': 'QR-Code Ordner',

resources/lang/en.js
@@ -84,6 +84,7 @@ const L10N = {
     'previewFromCam': 'See preview by device cam',
     'previewCamTakesPic': 'Device cam takes picture',
     'newest_first': 'Show latest images first',
+    'image_preview_before_processing': 'Preload and show image during filter processing',
     'folders_images': 'Image Folder',
     'folders_thumbs': 'Thumbnail Folder',
     'folders_qrcodes': 'QR-Code Folder',

resources/lang/es.js
@@ -84,6 +84,7 @@ const L10N = {
     'previewFromCam': 'Ver el vistazo por cámara',
     'previewCamTakesPic': 'Device cam takes picture',
     'newest_first': 'Primero muestre las últimas páginas',
+    'image_preview_before_processing': 'Preload image before processing',
     'folders_images': 'Carpeta de imágenes',
     'folders_thumbs': 'Carpeta de viñetas',
     'folders_qrcodes': 'Carpeta còdigo QR',

resources/lang/fr.js
@@ -84,6 +84,7 @@ const L10N = {
     'previewFromCam': 'Voir l\'aperçu par caméra',
     'previewCamTakesPic': 'Device cam takes picture',
     'newest_first': 'Affichez les dernières images en premier',
+    'image_preview_before_processing': 'Preload image before processing',
     'folders_images': 'Dossier d\'images',
     'folders_thumbs': 'Dossier de vignettes',
     'folders_qrcodes': 'Dossier QR code',

resources/lang/gr.js
@@ -84,6 +84,7 @@ const L10N = {
     'previewFromCam': 'Προβολή προεπισκόπησης από τη φωτογραφική μηχανή tablet',
     'previewCamTakesPic': 'Device cam takes picture',
     'newest_first': 'Προβολή των τελευταίων φωτογραφιών πρώτα',
+    'image_preview_before_processing': 'Preload image before processing',
     'folders_images': 'Φάκελος εικόνων',
     'folders_thumbs': 'Φάκελος μικρογραφιών',
     'folders_qrcodes': 'Φάκελο κώδικα QR',